fe70bf0c7d9d924cc6a87e2df23dc38894233121
Matheus Marin <matheus.marin@usp.br>
2020-03-23 15:15:43 -0700

    add param in url

--- spring-application-with-rabbitmq/src/main/java/controller/SpringController.java
+++ spring-application-with-rabbitmq/src/main/java/controller/SpringController.java
@@ -1,12 +1,13 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringController {
     @GetMapping("/greeting")
-    public String greeting() {
-        return "b";
+    public String greeting(@RequestParam("name") String name) {
+        return name;
     }
 }
