acb17fd254911382065fead682b9a3e2e98a287b
Matheus Marin <matheus.marin@usp.br>
2020-03-23 15:03:33 -0700

    fix hello world and add gitignore file

--- spring-application-with-rabbitmq/src/main/java/controller/SpringController.java
+++ spring-application-with-rabbitmq/src/main/java/controller/SpringController.java
@@ -1,18 +1,12 @@
 package controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class SpringController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        System.out.println("Request Received");
-        model.addAttribute("name", name);
-        return "greeting.html";
+    public String greeting() {
+        return "b";
     }
 }
--- spring-application-with-rabbitmq/src/main/java/main/ApplicationMain.java
+++ spring-application-with-rabbitmq/src/main/java/main/ApplicationMain.java
@@ -3,11 +3,13 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+@ComponentScan (basePackages = {"controller"})
+public class ApplicationMain {
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        SpringApplication.run(ApplicationMain.class, args);
     }
 
 }
