4910b2fb9f6c243f0befb9901296a767945c6e1d
Matheus Marin <matheus.marin@usp.br>
2020-03-23 18:29:11 -0700

    removing not used imports

--- spring-application-with-rabbitmq/src/main/java/services/RabbitMQConfigurations.java
+++ spring-application-with-rabbitmq/src/main/java/services/RabbitMQConfigurations.java
@@ -1,12 +1,10 @@
 package services;
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
